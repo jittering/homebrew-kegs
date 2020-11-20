32a49716de1999bd32a9ed90a9016bc453fd325d
Chetan Sarva <chetan@pixelcop.net>
2020-11-20 15:18:54 -0500

    vproxy: move to new home; add launchd service

--- Formula/vproxy.rb
+++ Formula/vproxy.rb
@@ -1,23 +1,57 @@
 class Vproxy < Formula
   desc "zero-config virtual proxies with tls"
-  homepage "https://github.com/chetan/vproxy"
-  version "0.3"
+  homepage "https://github.com/jittering/vproxy"
+  version "0.3.0"
 
   if OS.mac?
-    url "https://github.com/chetan/simpleproxy/releases/download/v#{version}/vproxy-macos-x64", :using => :nounzip
-    sha256 "597c13acf672b0cd66f1a4e82367ad016d87ae58dff98ac4b4524ece35757f16"
+    url "https://github.com/jittering/vproxy/releases/download/v#{version}/vproxy_#{version}_Darwin_x86_64.tar.gz"
+    sha256 "71cc72980c85e7b7a8f2fe126729bd0eb80377a57bdd274b012d9cc824f5dd88"
   elsif OS.linux?
-    url "https://github.com/chetan/simpleproxy/releases/download/v#{version}/vproxy-linux-x64"
-    sha256 "59fb70499801c2e90ded7de4b7335b32d8f23d9ce37c184a1dd784c1b01aa612"
+    url "https://github.com/jittering/vproxy/releases/download/v#{version}/vproxy_#{version}_Linux_x86_64.tar.gz"
+    sha256 "58167e289f8c6dc04613649dc4e88197b321d8c146afd0783a7e58d4058a4e06"
   end
 
   def install
-    if OS.mac?
-      bin.install "vproxy-macos-x64" => "vproxy"
-    elsif OS.linux?
-      bin.install "vproxy-linux-x64" => "vproxy"
+    bin.install "vproxy" => "vproxy"
+
+    File.open("#{etc}/vproxy.conf", "w") do |f|
+      f.puts <<-EOF
+# Sample config file
+EOF
     end
   end
+
+  plist_options :startup => true
+
+	def plist
+		<<~EOS
+			<?xml version="1.0" encoding="UTF-8"?>
+			<!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
+			<plist version="1.0">
+				<dict>
+					<key>KeepAlive</key>
+					<dict>
+						<key>SuccessfulExit</key>
+						<false/>
+					</dict>
+					<key>Label</key>
+					<string>#{plist_name}</string>
+					<key>ProgramArguments</key>
+					<array>
+						<string>#{opt_bin}/vproxy</string>
+					</array>
+					<key>RunAtLoad</key>
+					<true/>
+					<key>WorkingDirectory</key>
+					<string>#{var}</string>
+					<key>StandardErrorPath</key>
+					<string>#{var}/log/vproxy.log</string>
+					<key>StandardOutPath</key>
+					<string>#{var}/log/vproxy.log</string>
+				</dict>
+			</plist>
+		EOS
+	end
 
   test do
     system "vproxy", "--help"
